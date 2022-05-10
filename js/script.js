14414c1c6d48480c31ad36382cdd38c549d8937f
maikalangi <maikalangi@gmail.com>
2022-05-10 09:56:29 -0700

    commits intro sentence and tests markdown effects

--- js/script.js
+++ js/script.js
@@ -1,42 +1,38 @@
-// alert('js connected')
+// const promise = $.ajax({
+//     url:'https://audius-discovery-7.cultur3stake.com/v1/tracks/search?query=baauer'
+// });
 
-const promise = $.ajax({
-    url:'https://audius-discovery-7.cultur3stake.com/v1/tracks/search?query=baauer'
-});
+// const $art = $('img');
 
-const $art = $('img');
-const $song = $('source');
+// promise.then(
+//     (res) => {
+//     const artwork = res.data[0].artwork['480x480'];
+//     $art.attr('src', artwork);
+//     },
+    // (error) => {
+    //  console.log('bad request: ', error);
+    // }
+// );
 
-promise.then(
-    (res) => {
-    //  $des.text(data, description);
-    const artwork = res.data[0].artwork['480x480'];
-    $art.attr('src', artwork);
-    },
-    (error) => {
-     console.log('bad request: ', error);
-    }
-);
+const sample = (arr) => arr[Math.floor(Math.random() * arr.length)]
+const host = $.ajax('https://api.audius.co');
+console.log(host);
 
-// const arr = {
-//     150x150: 'https://content-node.audius.co/ipfs/QmVCwBE7STz7BFoKru43uukW3qvSpHgzpWLp5EkatMMD8q/150x150.jpg', 480x480: 'https://content-node.audius.co/ipfs/QmVCwBE7STz7BFoKru43uukW3qvSpHgzpWLp5EkatMMD8q/480x480.jpg', 1000x1000: 'https://content-node.audius.co/ipfs/QmVCwBE7STz7BFoKru43uukW3qvSpHgzpWLp5EkatMMD8q/1000x1000.jpg'
+// function handleGetData () {
+    
+//     // event.preventDefault();
+
+//     $.ajax(url).then((res) => {
+//         return sample(res.data);
+//         // console.log(host);
+//     },
+//     (error) => {
+//         console.log('bad request', error);
+//     })
 // };
 
+// $(document).ready(handleGetData);
 
 
-// const connect = promise.description;
-// console.log(connect);
-// $('body').append(connect);
 
-fetch('https://audius-discovery-12.cultur3stake.com/v1/tracks/D7KyD/stream?app_name=EXAMPLEAPP',
-{
-  method: 'GET'
-
-})
-.then(function(res) {
-    return res.json();
-}).then(function(body) {
-    const song = body;
-    console.log(song);
-    // $song.attr('src', song);
-});
+// console.log(sample(res.data));
