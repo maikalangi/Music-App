1af0c25d7d76b0fb79c548da7bfead54f885ece9
maikalangi <maikalangi@gmail.com>
2022-05-31 00:54:05 -0700

    wip: creates RESTful routes in controller

--- controllers/search.js
+++ controllers/search.js
@@ -1,11 +1,46 @@
 // DEPENDENCIES
+const { Router } = require('express');
 const express = require('express');
+const res = require('express/lib/response');
 
 // INITIALIZE
 const router = express.Router();
 
+// ROUTES
+
+// INDEX
 router.get('/', (req, res) => {
     res.render('index.ejs');
+});
+
+// NEW
+router.get('/new', (req, res) => {
+    res.render('new.ejs');
+});
+
+// DELETE
+router.delete('/:id', (req, res) => {
+    res.redirect('/');
+});
+
+// UPDATE
+router.put('/:id', (req, res) => {
+    res.redirect('/:id');
+});
+
+// CREATE
+router.post('/', (req, res) => {
+    res.redirect('/');
+});
+
+// EDIT
+router.get('/:id/edit', (req, res) => {
+    res.render('edit.ejs');
+});
+
+// SHOW
+router.get('/:id', (req, res) => {
+    res.render('show.ejs');
 });
 
 module.exports = router;
